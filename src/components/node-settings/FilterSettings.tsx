379761241fbb553237e5b3d15316c9a9ea2f8473
roelkers <rufus.oelkers@gmail.com>
2020-10-04 15:07:39 +0200

    adjust other audio node settings to use log/exp scale when needed

--- src/components/node-settings/FilterSettings.tsx
+++ src/components/node-settings/FilterSettings.tsx
@@ -1,170 +1,144 @@
-import { Grid, Input, InputLabel, Slider } from '@material-ui/core';
+import { Grid, Input, InputLabel, Slider, Tooltip } from '@material-ui/core';
 import React, { ChangeEvent } from 'react'
 import { SettingsProps } from '.';
+import { roundToTwoDecimals } from '../../functions/geometry';
 import useGridStyles from '../../hooks/useGridStyles'
 import { FilterConfig } from '../../nodeCreators/filter';
 import SettingsCollapse from '../SettingsCollapse';
+interface Props {
+  children: React.ReactElement;
+  open: boolean;
+  value: number;
+}
+function ValueLabelComponent(props: Props) {
+  const { children, open, value } = props;
+
+  return (
+    <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
+      {children}
+    </Tooltip>
+  );
+}
 
 export default function FilterSettings(props: SettingsProps<FilterConfig>) {
   const { handleSetParams, params, virtualAudioNodeIndex, nodeId } = props
   const { attack, release, frequency, resonance, envAmount } = params
+  const attackSlider = Math.log(attack + 1)
+  const releaseSlider = Math.log(release + 1)
+  const frequencySlider = (Math.log(frequency/100 +1)) 
+  const envAmountSlider = (Math.log(envAmount/100 +1)) 
+  const resonanceSlider = (Math.log(resonance/10 +1)) 
   const classes = useGridStyles()
-  const handleSliderChange = (param: string) => (e: ChangeEvent<{}>, newValue: number | number[]) =>
+  const handleSliderChange = (param: string) => (newValue: number) => {
     handleSetParams({ ...params, [param]: newValue })
-  const handleInputChange = (param: string) => (e: React.ChangeEvent<HTMLInputElement>) =>
-    handleSetParams({ ...params, [param]: Number(e.target.value) })
+  }
+  const lengthSliderFunc = (x: number) => Math.exp(x) - 1
+  const resonanceSliderFunc = (x: number) => 10* (Math.exp(x) - 1)
+  const frequencySliderFunc = (x: number) => 100* ( Math.exp(x) - 1)
   return (
     <SettingsCollapse title='Filter' nodeId={nodeId} virtualAudioNodeIndex={virtualAudioNodeIndex}>
-    <Grid className={classes.grid} container spacing={2} >
-      <Grid item xs >
-        <InputLabel className={classes.label}>
-          Attack
+      <Grid className={classes.grid} container spacing={2} >
+        <Grid item xs >
+          <InputLabel className={classes.label}>
+            Attack
          </InputLabel>
-        <Slider
-          value={attack}
-          onChange={handleSliderChange('attack')}
-          aria-labelledby="input-slider"
-          min={0}
-          max={10}
-          step={0.1}
-        />
+          <Slider
+            onChange={(e: any, newValue: number | number[]) =>
+              handleSliderChange('attack')
+                (lengthSliderFunc(newValue as number))}
+            valueLabelDisplay="auto"
+            valueLabelFormat={roundToTwoDecimals}
+            aria-labelledby="input-slider"
+            min={0}
+            scale={lengthSliderFunc}
+            max={2}
+            step={0.01}
+            value={attackSlider}
+          />
+        </Grid>
       </Grid>
-      <Grid item >
-        <Input
-          className={classes.input}
-          value={attack}
-          margin="dense"
-          onChange={handleInputChange('attack')}
-          inputProps={{
-            step: 0.1,
-            min: 0,
-            max: 10,
-            type: 'number',
-            'aria-labelledby': 'input-slider',
-          }}
-        />
-      </Grid>
-    </Grid>
-    <Grid className={classes.grid} container spacing={2} >
-      <Grid item xs >
-        <InputLabel className={classes.label}>
-          Release 
+      <Grid className={classes.grid} container spacing={2} >
+        <Grid item xs >
+          <InputLabel className={classes.label}>
+            Release
          </InputLabel>
-        <Slider
-          value={release}
-          onChange={handleSliderChange('release')}
-          aria-labelledby="input-slider"
-          min={0}
-          max={10}
-          step={0.1}
-        />
+          <Slider
+            onChange={(e: any, newValue: number | number[]) =>
+              handleSliderChange('release')
+                (lengthSliderFunc(newValue as number))}
+            valueLabelDisplay="auto"
+            valueLabelFormat={roundToTwoDecimals}
+            aria-labelledby="input-slider"
+            min={0}
+            scale={lengthSliderFunc}
+            max={2}
+            step={0.01}
+            value={releaseSlider}
+          />
+        </Grid>
       </Grid>
-      <Grid item >
-        <Input
-          className={classes.input}
-          value={release}
-          margin="dense"
-          onChange={handleInputChange('release')}
-          inputProps={{
-            step: 0.1,
-            min: 0,
-            max: 10,
-            type: 'number',
-            'aria-labelledby': 'input-slider',
-          }}
-        />
-      </Grid>
-    </Grid>
-    <Grid className={classes.grid} container spacing={2} >
-      <Grid item xs >
-        <InputLabel className={classes.label}>
-           Frequency 
+      <Grid className={classes.grid} container spacing={2} >
+        <Grid item xs >
+          <InputLabel className={classes.label}>
+            Frequency
          </InputLabel>
-        <Slider
-          value={frequency}
-          onChange={handleSliderChange('frequency')}
-          aria-labelledby="input-slider"
-          min={0}
-          max={20000}
-          step={10}
-        />
+          <Slider
+            onChange={(e: any, newValue: number | number[]) =>
+              handleSliderChange('frequency')
+                (frequencySliderFunc(newValue as number))}
+            valueLabelDisplay="auto"
+            valueLabelFormat={(v) => `${Math.ceil(v)}hz`}
+            ValueLabelComponent={ValueLabelComponent}
+            aria-labelledby="input-slider"
+            min={0}
+            scale={frequencySliderFunc}
+            max={5.25}
+            step={0.005}
+            value={frequencySlider}
+          />
+        </Grid>
       </Grid>
-      <Grid item >
-        <Input
-          className={classes.input}
-          value={frequency}
-          margin="dense"
-          onChange={handleInputChange('frequency')}
-          inputProps={{
-            step: 10,
-            min: 0,
-            max: 20000,
-            type: 'number',
-            'aria-labelledby': 'input-slider',
-          }}
-        />
-      </Grid>
-    </Grid>
-    <Grid className={classes.grid} container spacing={2} >
-      <Grid item xs >
-        <InputLabel className={classes.label}>
-           Resonance 
+      <Grid className={classes.grid} container spacing={2} >
+        <Grid item xs >
+          <InputLabel className={classes.label}>
+            Resonance
          </InputLabel>
-        <Slider
-          value={resonance}
-          onChange={handleSliderChange('resonance')}
-          aria-labelledby="input-slider"
-          min={0}
-          max={100}
-          step={1}
-        />
+          <Slider
+            onChange={(e: any, newValue: number | number[]) =>
+              handleSliderChange('resonance')
+                (resonanceSliderFunc(newValue as number))}
+            valueLabelDisplay="auto"
+            valueLabelFormat={roundToTwoDecimals}
+            aria-labelledby="input-slider"
+            min={0}
+            scale={resonanceSliderFunc}
+            max={2}
+            step={0.005}
+            value={resonanceSlider}
+          />
+        </Grid>
       </Grid>
-      <Grid item >
-        <Input
-          className={classes.input}
-          value={resonance}
-          margin="dense"
-          onChange={handleInputChange('resonance')}
-          inputProps={{
-            step: 1,
-            min: 0,
-            max: 100,
-            type: 'number',
-            'aria-labelledby': 'input-slider',
-          }}
-        />
-      </Grid>
-    </Grid>
-    <Grid className={classes.grid} container spacing={2} >
-      <Grid item xs >
-        <InputLabel className={classes.label}>
-           Envelope Amount
+      <Grid className={classes.grid} container spacing={2} >
+        <Grid item xs >
+          <InputLabel className={classes.label}>
+            Envelope Amount 
          </InputLabel>
-        <Slider
-          value={envAmount}
-          onChange={handleSliderChange('envAmount')}
-          aria-labelledby="input-slider"
-          min={0}
-          max={20000}
-          step={10}
-        />
+          <Slider
+            onChange={(e: any, newValue: number | number[]) =>
+              handleSliderChange('envAmount')
+                (frequencySliderFunc(newValue as number))}
+            valueLabelDisplay="auto"
+            valueLabelFormat={(v) => `${Math.ceil(v)}hz`}
+            ValueLabelComponent={ValueLabelComponent}
+            aria-labelledby="input-slider"
+            min={0}
+            scale={frequencySliderFunc}
+            max={5.25}
+            step={0.005}
+            value={envAmountSlider}
+          />
+        </Grid>
       </Grid>
-      <Grid item >
-        <Input
-          className={classes.input}
-          value={envAmount}
-          margin="dense"
-          onChange={handleInputChange('envAmount')}
-          inputProps={{
-            step: 10,
-            min: 0,
-            max: 20000,
-            type: 'number',
-            'aria-labelledby': 'input-slider',
-          }}
-        />
-      </Grid>
-    </Grid>
     </SettingsCollapse>
   )
 }
--- src/components/node-settings/OscSettings.tsx
+++ src/components/node-settings/OscSettings.tsx
@@ -2,6 +2,7 @@ import { Grid, Input, InputLabel, Slider } from '@material-ui/core';
 import React, { ChangeEvent } from 'react'
 import { useSelector } from 'react-redux';
 import { SettingsProps } from '.';
+import { roundToTwoDecimals } from '../../functions/geometry';
 import useGridStyles from '../../hooks/useGridStyles'
 import { OscConfig } from '../../nodeCreators/osc';
 import { selectScale } from '../../slices/global-audio';
@@ -9,110 +10,114 @@ import SettingsCollapse from '../SettingsCollapse';
 
 export default function OscSettings(props: SettingsProps<OscConfig>) {
   const { handleSetParams, params, nodeId, virtualAudioNodeIndex } = props
-  const { attack, release, scaleNoteIndex, octave } = params
+  const { attack, release, scaleNoteIndex, envFrequencyAmount, octave } = params
   const scale = useSelector(selectScale)
   const classes = useGridStyles()
-  const handleSliderChange = (param: string) => (e: ChangeEvent<{}>, newValue: number | number[]) =>
+  const attackSlider = Math.log(attack + 1)
+  const releaseSlider = Math.log(release + 1)
+  const handleSliderChange = (param: string) => (newValue: number) => {
     handleSetParams({ ...params, [param]: newValue })
+  }
   const handleInputChange = (param: string) => (e: React.ChangeEvent<HTMLInputElement>) =>
     handleSetParams({ ...params, [param]: Number(e.target.value) })
+  const lengthSliderFunc = (x: number) => Math.exp(x) - 1
   const noteMarks = scale.map((note: string, index: number) => ({ value: index, label: note }))
   return (
     <SettingsCollapse title='Oscillator' nodeId={nodeId} virtualAudioNodeIndex={virtualAudioNodeIndex}>
-    <Grid className={classes.grid} container spacing={2} >
-      <Grid item xs >
-        <InputLabel className={classes.label}>
-          Attack 
+      <Grid className={classes.grid} container spacing={2} >
+        <Grid item xs >
+          <InputLabel className={classes.label}>
+            Note
          </InputLabel>
-        <Slider
-          value={attack}
-          onChange={handleSliderChange('attack')}
-          aria-labelledby="input-slider"
-          min={0}
-          max={10}
-          step={0.1}
-        />
-      </Grid>
-      <Grid item >
-        <Input
-          className={classes.input}
-          value={attack}
-          margin="dense"
-          onChange={handleInputChange('attack')}
-          inputProps={{
-            step: 0.1,
-            min: 0,
-            max: 10,
-            type: 'number',
-            'aria-labelledby': 'input-slider',
-          }}
-        />
-      </Grid>
-    </Grid>
-    <Grid className={classes.grid} container spacing={2} >
-      <Grid item xs >
-        <InputLabel className={classes.label}>
-          Release 
+          <Slider
+            value={scaleNoteIndex}
+            onChange={(e: any, newValue: number | number[]) =>
+              handleSliderChange('scaleNoteIndex')(newValue as number)}
+            aria-labelledby="input-slider"
+            min={0}
+            marks={noteMarks}
+            max={7}
+            step={1}
+          />
+        </Grid>
+        <Grid item >
+          <InputLabel className={classes.label}>
+            Octave
          </InputLabel>
-        <Slider
-          value={release}
-          onChange={handleSliderChange('release')}
-          aria-labelledby="input-slider"
-          min={0}
-          max={10}
-          step={0.1}
-        />
+          <Input
+            className={classes.input}
+            value={octave}
+            margin="dense"
+            onChange={handleInputChange('octave')}
+            inputProps={{
+              step: 1,
+              min: 0,
+              max: 8,
+              type: 'number',
+              'aria-labelledby': 'input-slider',
+            }}
+          />
+        </Grid>
       </Grid>
-      <Grid item >
-        <Input
-          className={classes.input}
-          value={release}
-          margin="dense"
-          onChange={handleInputChange('release')}
-          inputProps={{
-            step: 0.1,
-            min: 0,
-            max: 10,
-            type: 'number',
-            'aria-labelledby': 'input-slider',
-          }}
-        />
-      </Grid>
-    </Grid>
-    <Grid className={classes.grid} container spacing={2} >
-      <Grid item xs >
-        <InputLabel className={classes.label}>
-           Note 
+      <Grid className={classes.grid} container spacing={2} >
+        <Grid item xs >
+          <InputLabel className={classes.label}>
+            Attack
          </InputLabel>
-        <Slider
-          value={scaleNoteIndex}
-          onChange={handleSliderChange('scaleNoteIndex')}
-          aria-labelledby="input-slider"
-          min={0}
-          marks={noteMarks}
-          max={7}
-          step={1}
-        />
+          <Slider
+            onChange={(e: any, newValue: number | number[]) =>
+              handleSliderChange('attack')
+                (lengthSliderFunc(newValue as number))}
+            valueLabelDisplay="auto"
+            valueLabelFormat={roundToTwoDecimals}
+            aria-labelledby="input-slider"
+            min={0}
+            scale={lengthSliderFunc}
+            max={2}
+            step={0.01}
+            value={attackSlider}
+          />
+        </Grid>
       </Grid>
-      <Grid item >
-        <InputLabel className={classes.label}>
-           Octave 
+      <Grid className={classes.grid} container spacing={2} >
+        <Grid item xs >
+          <InputLabel className={classes.label}>
+            Release
          </InputLabel>
-        <Input
-          className={classes.input}
-          value={octave}
-          margin="dense"
-          onChange={handleInputChange('octave')}
-          inputProps={{
-            step: 1,
-            min: 0,
-            max: 8,
-            type: 'number',
-            'aria-labelledby': 'input-slider',
-          }}
-        />
+          <Slider
+            onChange={(e: any, newValue: number | number[]) =>
+              handleSliderChange('release')
+                (lengthSliderFunc(newValue as number))}
+            valueLabelDisplay="auto"
+            valueLabelFormat={roundToTwoDecimals}
+            aria-labelledby="input-slider"
+            min={0}
+            scale={lengthSliderFunc}
+            max={2}
+            step={0.01}
+            value={releaseSlider}
+          />
+        </Grid>
       </Grid>
-    </Grid>
+      <Grid className={classes.grid} container spacing={2} >
+        <Grid item xs >
+          <InputLabel className={classes.label}>
+            Pitch Env Amount 
+         </InputLabel>
+          <Slider
+            onChange={(e: any, newValue: number | number[]) =>
+              handleSliderChange('envFrequencyAmount')
+                (newValue as number)}
+            valueLabelDisplay="auto"
+            valueLabelFormat={roundToTwoDecimals}
+            aria-labelledby="input-slider"
+            min={-100}
+            max={100}
+            step={0.01}
+            value={envFrequencyAmount}
+          />
+        </Grid>
+      </Grid>
     </SettingsCollapse>
   )
 }
